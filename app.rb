ebe5cc3f0b3029f59ca39927fe57a72c1e42cb68
Mike Lofgren <mikeylofgren@gmail.com>
2019-08-02 15:14:14 -0700

    work on erb
    .

--- app.rb
+++ app.rb
@@ -1,3 +1,18 @@
 require("pg")
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/project')
+require('./lib/volunteer')
+require('pry')
+also_reload('lib/**/*.rb')
 
 DB = PG.connect({:dbname => "volunteer_tracker"})
+
+get('/') do
+  redirect to('/projects')
+end
+
+get('/projects') do
+  @projects = Project.all
+  erb :projects 
+end
