59d7778a4e9683a38ab35852eb0e79d966f86f34
Mike Lofgren <mikeylofgren@gmail.com>
2019-08-02 14:14:41 -0700

    pass volunteer save spec.

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -1,11 +1,11 @@
 class Volunteer
   attr_accessor :name, :project_id, :id
 
-def initialize(attributes)
-  @name = attributes.fetch(:name)
-  @project_id = attributes.fetch(:project_id)
-  @id = attributes.fetch(:id)
-end
+  def initialize(attributes)
+    @name = attributes.fetch(:name)
+    @project_id = attributes.fetch(:project_id)
+    @id = attributes.fetch(:id)
+  end
 
   def == volunteer_to_compare
     if volunteer_to_compare != nil
@@ -14,4 +14,22 @@ end
       false
     end
   end
+
+  def self.all
+    returned_volunteers = DB.exec("SELECT * FROM volunteers ORDER BY name;")
+    volunteers = []
+    returned_volunteers.each do |volunteer|
+      name = volunteer.fetch("name")
+      project_id = volunteer.fetch("project_id").to_i
+      id = volunteer.fetch("id").to_i
+      volunteers.push(Volunteer.new({:name => name, :project_id => project_id, :id => id}))
+    end
+    volunteers
+  end
+
+  def save
+    result = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}', '#{@project_id}') RETURNING id;")
+    @id = result.first.fetch("id").to_i
+  end
+
 end
