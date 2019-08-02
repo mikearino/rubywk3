37eee66cd7fc975272eb2a8d0f5de39ee9016aae
Mike Lofgren <mikeylofgren@gmail.com>
2019-08-02 13:34:07 -0700

    pass volunteer name spec.

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -1,16 +1,16 @@
 class Volunteer
   attr_accessor :name, :project_id, :id
-end
 
 def initialize(attributes)
   @name = attributes.fetch(:name)
   @project_id = attributes.fetch(:project_id)
   @id = attributes.fetch(:id)
-
+end
   def == volunteer_to_compare
     if volunteer_to_compare != nil
       (self.name == volunteer_to_compare.name) && (self.project_id == volunteer_to_compare.project_id)
     else
       false
     end
-  end 
+  end
+end
